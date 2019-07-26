cd42da2c5e1cbc6b7439e7e6e9e10df0843b819b
ilyeral <41276076+ilyeral@users.noreply.github.com>
2019-07-26 10:07:29 +0800

    Update FileSQL.h

--- FileSQL.h
+++ FileSQL.h
@@ -7,28 +7,51 @@
 #include "sql.h"
 #include "sqlext.h"
 #include "sqltypes.h"
-
+using namespace std;
 
 class FileSQL {
 public:
-	SQLHENV henv = SQL_NULL_HENV;
-	SQLHDBC hdbc = SQL_NULL_HDBC;
+	SQLHENV henv;
+	SQLHDBC hdbc;
 	SQLRETURN ret;
-	SQLWCHAR dsn[64] = L"ems_data";
-	SQLWCHAR user[64] = L"scada";
-	SQLWCHAR password[64] = L"scadaems";
+	SQLWCHAR *dsn;
+	SQLWCHAR *user;
+	SQLWCHAR *password;
 
 	FileSQL();
+	FileSQL(string s_dsn, string s_user, string s_password);
 	void Open();
 	void Close();
 
-	bool Insert(char *filePath);
-	bool Update();
-	bool Delete();
-	bool Select();
+	bool Insert(char* filePath);
+	bool Update(char* filePath);
+	bool Delete(char* id);
+	bool SelectById(char *id);
+	bool SelectAll();
+
+	bool TableExistence(); 
+	bool ScriptTableExistence();
+	bool BindDataBase(string ip, string user, string password);
+	bool UnBindDataBase(string name);
+	bool SelectAllBind();
+
+	void CreateTriggerForBind(string bindName, string dbName);
+	bool CreateI_TriggerForBind(string bindName, string dbName);
+	bool CreateD_TriggerForBind(string bindName, string dbName);
+	bool CreateU_TriggerForBind(string bindName, string dbName);
+
+
+	void DropTriggerForBind(string bindName);
+	bool DropI_TriggerForBind(string bindName);
+	bool DropD_TriggerForBind(string bindName);
+	bool DropU_TriggerForBind(string bindName);
+
+	bool DoScript();
+	int ExecuteScript(int id, char* fName, char* fType, long fSize, char* data, char* srvr, int operation, char* dbName);
+	bool ClearScriptBySrvr(string srvr);
+	bool ClearScriptById(int di);
+
+
 
 	void GetHstmtErrorMsg(SQLHSTMT hstmt);
-
-
-
 };
